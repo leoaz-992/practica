f40a7baf392a5f96ca9c4701660909d7cdf7380f
Leonardo Alvarez <leo92alvarez25@gmail.com>
2023-09-19 17:42:47 -0300

    validaciones con js

--- index.php
+++ index.php
@@ -11,24 +11,24 @@
     </div>
     <?php session_unset(); } ?>
     <div class="card card-body mt-2">
-      <form action="save_contact.php" method="post">
+      <form  action="save_contact.php" method="post">
         <input class="form-control my-2" type="text" name="nombre" id="nombre" placeholder="ingrese su nombre" autofocus>
         <input class="form-control my-2" type="email" name="email" id="email" placeholder="ingrese su email">
         <textarea class="form-control" name="mensaje" id="mensaje" cols="30" rows="4"></textarea>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end my-2">
-          <input class="btn btn-primary" name="save_contact" type="submit" value="enviar Datos">
+          <input class="btn btn-primary" id="botonSubmit" name="save_contact" type="submit" value="enviar Datos">
         </div>
       </form>
     </div>
   </div>
-  <div class="col-md-8">
+  <div class="col-md-8 pe-0">
     <table class="table table-dark table-striped pt-2 mt-2">
       <thead>
         <tr>
           <th scope="col">ID</th>
           <th scope="col">Nombre</th>
           <th scope="col">Correo</th>
-          <th scope="col">Actions</th>
+          <th scope="col" class="text-center">Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -42,9 +42,9 @@
             <th scope='row'><?php echo $contacto_data['tabla01_ID']?></th>
             <td><?php echo $contacto_data['tabla01_nombre']?></td>
             <td><?php echo $contacto_data['tabla01_email']?></td>
-            <td>
-              <a class='btn btn-info btn-sm me-3' href="editar_contacto.php?id=<?=$contacto_data['tabla01_ID']?>">EDITAR</a>
-              <a class='btn btn-danger btn-sm ' href="eliminar_contacto.php?id=<?=$contacto_data['tabla01_ID']?>">ELIMINAR</a>
+            <td class="row justify-content-around pe-0 me-0">
+              <a class='col-md-5 btn btn-info btn-sm' href="editar_contacto.php?id=<?=$contacto_data['tabla01_ID']?>">EDITAR</a>
+              <a class='col-md-5 btn btn-danger btn-sm ' href="eliminar_contacto.php?id=<?=$contacto_data['tabla01_ID']?>">ELIMINAR</a>
             </td>
           </tr>
           
